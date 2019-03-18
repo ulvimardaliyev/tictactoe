868ecdf5e390cdac72c0f8eed0d0e8bc9d3f6895
ulvi <merdeliyev@inbox.ru>
2019-03-18 23:43:20 +0400

    moved codes from main() to relevant method

--- src/tictactoe/Run.java
+++ src/tictactoe/Run.java
@@ -9,54 +9,16 @@ public class Run {
     private static String playerInputX_O;
     private String playerEnteredPosition;
     static int playedGameCount;
-    private String dividePlayerEnteredPositionToRowAndGameElement[];
+    private String dividePlayerEnteredPositionToRowAndColumn[];
     private static final String regex = "[1-3][,][\\s][A-C]";
 
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe game.");
-        System.out.println("Please, select an element to start a game: which do you want? X or O");
-        Scanner scanner = new Scanner(System.in);
         Run run = new Run();
-        Elements.getRidOfNullElements();
-        run.createAnElementToStartAGame(scanner);
-        System.out.println(" " + " A" + " " + " " + " " + " B" + " " + " " + " " + " C");
-        String printer = "1" + "  " + Elements.getKeepPlayerEnteredPositions()[0][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[0][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[0][2] + "\n"
-                + " " + " " + " " + " " + " | " + " " + " " + "|\n"
-                + " - " + "- " + "- " + "- " + "- " + "- " + "- \n"
-                + "2" + " " + " " + Elements.getKeepPlayerEnteredPositions()[1][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[1][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[1][2] + "\n"
-                + " " + " " + " " + " " + " " + "|" + " " + " " + " " + "|\n"
-                + " - " + "- " + "- " + "- " + "- " + "- " + "- \n"
-                + "3" + " " + " " + Elements.getKeepPlayerEnteredPositions()[2][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[2][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[2][2] + "\n"
-                + " " + " " + " " + " " + " " + "|" + " " + " " + " " + "|\n";
-        System.out.println(printer);
-
-        Pattern pattern = Pattern.compile(regex);
-
-
-        for (playedGameCount = 0; playedGameCount < 5; playedGameCount++) {
-            while (true) {
-                run.playerEnteredPosition = scanner.nextLine();
-                Matcher matcher = pattern.matcher(run.playerEnteredPosition);
-                if (matcher.find()) {
-                    run.dividePlayerEnteredPositionToRowAndGameElement = run.playerEnteredPosition.split(", ");
-                    Elements e = new Elements(Integer.parseInt(run.dividePlayerEnteredPositionToRowAndGameElement[0]),
-                            run.dividePlayerEnteredPositionToRowAndGameElement[1], playerInputX_O);
-
-                    GameProcessing gameProcessing = new GameProcessing(e);
-                    gameProcessing.doProcess(playedGameCount);
-                    if (gameProcessing.winsUserOrComputer()) {
-                        System.out.println("You won");
-                        playedGameCount = 5;
-                    }
-                    break;
-                } else {
-                    System.err.println("Please, enter correct style: \"1, B\" or \"2, C\"");
-                }
-            }
-        }
+        run.startGame(run);
     }
 
-    public void createAnElementToStartAGame(Scanner scanner) {
+    public void chooseX_OToStartGame(Scanner scanner) {
+
         while (true) {
             playerInputX_O = scanner.next();
             if (playerInputX_O.equals("X")) {
@@ -69,5 +31,59 @@ public class Run {
                 System.err.println("No no no. Please, enter correct element, X or O");
             }
         }
+
+    }
+
+    public void startGame(Run run) {
+
+        System.out.println("Welcome to TicTacToe game.");
+        System.out.println("Please, select an element to start a game: which do you want? X or O");
+
+        Scanner scanner = new Scanner(System.in);
+
+        Elements.getRidOfNullElements();
+        run.chooseX_OToStartGame(scanner);
+
+        System.out.println(playground());
+
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+
+        for (playedGameCount = 0; playedGameCount < 5; playedGameCount++) {
+            while (true) {
+                run.playerEnteredPosition = scanner.nextLine();
+                matcher = pattern.matcher(run.playerEnteredPosition);
+                if (matcher.find()) {
+                    run.dividePlayerEnteredPositionToRowAndColumn = run.playerEnteredPosition.split(", ");
+                    Elements elements = new Elements(Integer.parseInt(run.dividePlayerEnteredPositionToRowAndColumn[0]),
+                            run.dividePlayerEnteredPositionToRowAndColumn[1], playerInputX_O);
+
+                    GameProcessing gameProcessing = new GameProcessing(elements);
+                    gameProcessing.doProcess(playedGameCount);
+
+                    if (gameProcessing.winsUserOrComputer()) {
+                        System.out.println("You won");
+                        playedGameCount = 5;
+                    }
+                    break;
+                } else {
+                    System.err.println("Please, enter correct style: \"1, B\" or \"2, C\"");
+                }
+            }
+        }
+    }
+
+    private String playground() {
+        String column = " " + " A" + " " + " " + " " + " B" + " " + " " + " " + " C";
+        String row = "1" + "  " + Elements.getKeepPlayerEnteredPositions()[0][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[0][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[0][2] + "\n"
+                + " " + " " + " " + " " + " | " + " " + " " + "|\n"
+                + " - " + "- " + "- " + "- " + "- " + "- " + "- \n"
+                + "2" + " " + " " + Elements.getKeepPlayerEnteredPositions()[1][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[1][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[1][2] + "\n"
+                + " " + " " + " " + " " + " " + "|" + " " + " " + " " + "|\n"
+                + " - " + "- " + "- " + "- " + "- " + "- " + "- \n"
+                + "3" + " " + " " + Elements.getKeepPlayerEnteredPositions()[2][0] + " " + "|" + " " + Elements.getKeepPlayerEnteredPositions()[2][1] + " " + "| " + Elements.getKeepPlayerEnteredPositions()[2][2] + "\n"
+                + " " + " " + " " + " " + " " + "|" + " " + " " + " " + "|\n";
+        return column + "\n" + row;
     }
 }
